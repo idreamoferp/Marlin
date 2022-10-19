01e444395b1485352e910c15d4ed54f2500feb1b
Justin Mangini <justin@idreamoferp.com>
2022-10-19 16:28:31 -0600

    no kill home sequence

--- Marlin/src/module/endstops.cpp
+++ Marlin/src/module/endstops.cpp
@@ -454,7 +454,7 @@ void Endstops::not_homing() {
   // If the last move failed to trigger an endstop, call kill
   void Endstops::validate_homing_move() {
     if (trigger_state()) hit_on_purpose();
-    else kill(GET_TEXT_F(MSG_KILL_HOMING_FAILED));
+    else SERIAL_ERROR_MSG("Homeing Failed");
   }
 #endif
 
